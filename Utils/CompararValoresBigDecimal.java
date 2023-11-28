0b16c42918c5f18adc063c0bfc196e97be6acbdf
JrDevCJ <109694054+JrDevCJ@users.noreply.github.com>
2023-11-28 16:04:10 -0300

    Update CompararValoresBigDecimal.java

--- Utils/CompararValoresBigDecimal.java
+++ Utils/CompararValoresBigDecimal.java
@@ -4,15 +4,40 @@ public class CompararValoresBigDecimal {
 
 	public static void main(String args[]) {
 
+		// Valor1 igual Valor2
 		BigDecimal valor1 = new BigDecimal("10.00");
-		BigDecimal valor2 = new BigDecimal("20.00");
+		BigDecimal valor2 = new BigDecimal("10.00");
+		int retornoCompareTo = valor1.compareTo(valor2);
 
-		if(valor1.compareTo(valor2) < 0){
-			System.out.print("Valor1 é menor Valor2");
+		System.out.println("retornoCompareTo: " + retornoCompareTo);
+
+		if (retornoCompareTo == 0) {
+			System.out.println("Valor1 é igual Valor2");
 		}
 
+		// Valor1 maior que Valor2
+		valor1 = new BigDecimal("20.00");
+		valor2 = new BigDecimal("10.00");
+		retornoCompareTo = valor1.compareTo(valor2);
 
+		System.out.println("retornoCompareTo: " + retornoCompareTo);
+
+		if (retornoCompareTo == 1) {
+			System.out.println("Valor1 é maior que Valor2");
+		}
+
+		// Valor1 maior que Valor2
+		valor1 = new BigDecimal("10.00");
+		valor2 = new BigDecimal("20.00");
+		retornoCompareTo = valor1.compareTo(valor2);
+
+		System.out.println("retornoCompareTo: " + retornoCompareTo);
+
+		if (retornoCompareTo == -1) {
+			System.out.println("Valor1 é menor que Valor2");
+		}
 
 	}
 
 }
+
